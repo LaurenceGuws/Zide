8f1df2c7c553552f2ba7a82fe8f175ceff96277a
Laurence <lggousie46@gmail.com>
2026-01-25 22:25:51 +0200

    Tests: widget action unions

---
+++ src/widget_action_tests.zig
@@ -0,0 +1,17 @@
+const std = @import("std");
+const types = @import("types/mod.zig");
+
+test "widget action unions compile" {
+    const alloc = std.testing.allocator;
+    _ = alloc;
+
+    const action = types.actions.WidgetAction{
+        .editor = .{ .request_save = {} },
+    };
+    _ = action;
+
+    const action2 = types.actions.WidgetAction{
+        .terminal = .{ .copy_selection = {} },
+    };
+    _ = action2;
+}
