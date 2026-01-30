cb0aec74f04d88dbb798c902965de0aa7d861732
LaurenceGouws <98704251+LaurenceGuws@users.noreply.github.com>
2026-01-31 00:29:15 +0200

    Fix window flags import path

--- src/ui/renderer/window_flags.zig
+++ src/ui/renderer/window_flags.zig
@@ -1,4 +1,4 @@
-const window_events = @import("../platform/window_events.zig");
+const window_events = @import("../../platform/window_events.zig");
 
 pub fn handleWindowEvent(event_id: u8, should_close: *bool, window_resized: *bool) void {
     if (window_events.isResizeEvent(event_id)) {
