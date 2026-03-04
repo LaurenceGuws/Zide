fab74c208642cb16a82fe86e9c43b6d1657ae821
Laurence <lggousie46@gmail.com>
2026-03-04 16:37:31 +0200

    app: fix ide mode helper re-exports for build

--- src/app/modes/ide/mod.zig
+++ src/app/modes/ide/mod.zig
@@ -1,1 +1,11 @@
-pub usingnamespace @import("host.zig");
+const host = @import("host.zig");
+
+pub const IdeHost = host.IdeHost;
+pub const ActiveMode = host.ActiveMode;
+pub const initialActiveMode = host.initialActiveMode;
+pub const initialTerminalVisibility = host.initialTerminalVisibility;
+pub const isTerminalOnly = host.isTerminalOnly;
+pub const isEditorOnly = host.isEditorOnly;
+pub const supportsEditorSurface = host.supportsEditorSurface;
+pub const supportsTerminalSurface = host.supportsTerminalSurface;
+pub const routedActiveMode = host.routedActiveMode;
