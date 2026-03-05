fa50179217714d62093c50b52861a0c6f544ad23
LaurenceGouws <98704251+LaurenceGuws@users.noreply.github.com>
2026-03-05 21:01:42 +0200

    config(lua): reuse shared emptyConfig in capi parser nil path

--- src/config/lua_config_capi.zig
+++ src/config/lua_config_capi.zig
@@ -211,47 +211,7 @@ pub fn parseConfigFromLuaState(allocator: std.mem.Allocator, L: *anyopaque) LuaC
 
 fn parseConfigFromStack(allocator: std.mem.Allocator, L: *c.lua_State) LuaConfigError!Config {
     if (c.lua_isnil(L, -1)) {
-        return .{
-            .log_file_filter = null,
-            .log_console_filter = null,
-            .sdl_log_level = null,
-            .editor_wrap = null,
-            .editor_large_jump_rows = null,
-            .editor_highlight_budget = null,
-            .editor_width_budget = null,
-            .app_font_path = null,
-            .app_font_size = null,
-            .editor_font_path = null,
-            .editor_font_size = null,
-            .editor_font_features = null,
-            .editor_disable_ligatures = null,
-            .terminal_font_path = null,
-            .terminal_font_size = null,
-            .terminal_blink_style = null,
-            .terminal_disable_ligatures = null,
-            .terminal_font_features = null,
-            .terminal_scrollback_rows = null,
-            .terminal_cursor_shape = null,
-            .terminal_cursor_blink = null,
-            .editor_tab_bar_width_mode = null,
-            .terminal_tab_bar_show_single_tab = null,
-            .terminal_tab_bar_width_mode = null,
-            .terminal_focus_report_window = null,
-            .terminal_focus_report_pane = null,
-            .font_lcd = null,
-            .font_hinting = null,
-            .font_autohint = null,
-            .font_glyph_overflow = null,
-            .text_gamma = null,
-            .text_contrast = null,
-            .text_linear_correction = null,
-            .theme = null,
-            .app_theme = null,
-            .editor_theme = null,
-            .terminal_theme = null,
-            .keybinds_no_defaults = null,
-            .keybinds = null,
-        };
+        return lua_shared.emptyConfig();
     }
     if (!c.lua_istable(L, -1)) {
         return LuaConfigError.InvalidConfig;
