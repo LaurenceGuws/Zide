4cb6df0a2311c64169bc4947c8caf02732bddb11
Laurence <lggousie46@gmail.com>
2026-03-06 20:19:34 +0200

    tools: drop removed dependency_path bootstrap report field

--- tools/build_bootstrap_report.zig
+++ tools/build_bootstrap_report.zig
@@ -4,7 +4,6 @@ const build_options = @import("build_options");
 pub fn main() !void {
     std.debug.print("build bootstrap\n", .{});
     std.debug.print("mode: {s}\n", .{build_options.build_mode});
-    std.debug.print("dependency_path: {s}\n", .{build_options.dependency_path});
     std.debug.print("renderer_backend: {s}\n", .{build_options.renderer_backend});
     std.debug.print("target_os: {s}\n", .{build_options.target_os});
     std.debug.print("use_vcpkg: {any}\n", .{build_options.use_vcpkg});
