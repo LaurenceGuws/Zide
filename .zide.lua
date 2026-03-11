ac83cffba5146e6b370801183213a6d3d784bf4a
Laurence <lggousie46@gmail.com>
2026-03-11 13:46:51 +0200

    debug: enable terminal redraw span logs

---
+++ .zide.lua
@@ -0,0 +1,13 @@
+return {
+    sdl = {
+        log_level = "warning",
+    },
+    log = {
+        console = "",
+        file = "terminal.ui.perf,terminal.ui.redraw",
+    },
+    logs = {
+        file_level = "info",
+        console_level = "info",
+    },
+}
