b9cc6cc46c4d864a21087132762d468fa99ee0fe
LaurenceGouws <98704251+LaurenceGuws@users.noreply.github.com>
2026-03-18 21:26:28 +0200

    Restrict project config to logging overrides

--- .zide.lua
+++ .zide.lua
@@ -1,20 +1,9 @@
 return {
-	editor = {
-		imported_theme = "kanagawa-dragon",
-	},
-	sdl = {
-		log_level = "info",
+	log = {
+		enable = {},
 	},
 	logs = {
-		file = {
-			"editor.highlight",
-			"editor.grammar",
-		},
 		file_level = "info",
 		console_level = "warning",
-		file_levels = {
-			["editor.highlight"] = "info",
-			["editor.grammar"] = "info",
-		},
 	},
 }
